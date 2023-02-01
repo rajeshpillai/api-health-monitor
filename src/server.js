e2b69b039dedc229ef31cae166113ea7973e9267
rajeshpillai <pillai.rajesh@gmail.com>
2023-02-01 19:39:20 +0530

    Flattened the api structure.  The source is at the same level.  Need to add the parent_id if needed

--- src/server.js
+++ src/server.js
@@ -21,9 +21,8 @@ let apiStatus = {};
 
 const apiEndpoints = [
   { id: 1, name: 'API 1', endpoint: 'http://api1.com/status' },
-  { id: 2, name: 'API 2', endpoint: 'https://jsonplaceholder.typicode.com/posts/1', sources: [
-    { id: 1.1, name: 'Source API 1', endpoint: 'https://api1.com/status' },
-  ] },
+  { id: 2, name: 'API 2', endpoint: 'https://jsonplaceholder.typicode.com/posts/1'},
+  { id: 2.1, name: 'Source API 1', endpoint: 'https://jsonplaceholder.typicode.com/posts/1/comments' },
   { id: 3, name: 'API 3', endpoint: 'https://jsonplaceholder.typicode.com/todos/1'},
   { id: 4, name: 'API 4', endpoint: 'https://jsonplaceholder.typicode.com/posts/1'},
   { id: 5, name: 'API 5', endpoint: 'http://api2.com/status' },
@@ -54,60 +53,6 @@ const monitorApi = async () => {
 
         io.emit('apiStatus', apiStatus);
         console.log(`Checking sources...`, sources);
-        if (sources) {
-          for (let s = 0; s < sources.length; s++) {
-            let source = sources[s];
-            console.log(`Checking source ${source.name} of ${name}...`);
-            try {
-              const response = await axios.get(source.endpoint);
-              if (response.status >= 200 && response.status < 300) {
-                apiStatus[source.id] = {
-                  id: source.id,
-                  name: source.name,
-                  status: '💚',
-                  endpoint: source.endpoint,
-                  responseTime: Date.now() - start,
-                  lastHealthy: new Date(),
-                  sources: []
-
-                };
-              } else {
-                apiStatus[source.id] = {
-                  id: source.id,
-                  name: source.name, 
-                  status: '❤️',
-                  endpoint: source.endpoint,
-                  responseTime: Date.now() - start,
-                  sources: []
-
-                };
-              }
-              io.emit('apiStatus', apiStatus);
-
-            } catch (e) {
-              console.log("ERROR: ", e.message);
-              apiStatus[source.id] = {
-                id: source.id,
-                name: source.name,
-                status: '❤️',
-                endpoint: source.endpoint,
-                error: e.message,
-                sources: []
-              };
-              io.emit('apiStatus', apiStatus);
-            }
-          }
-        }
-      } else {
-        apiStatus[id] = {
-          id: id,
-          name: name,
-          status: '💚',
-          endpoint: endpoint,
-          response: responseTime,
-          sources: sources
-        }
-        io.emit('apiStatus', apiStatus);
       }
     } catch (e) {
         console.log("ERROR: ", e.message);
